bc29f26ce1a50f96dc5bd2c545d4127665502604
Martin Aumüller <aumuell@reserv.at>
2026-04-23 22:28:06 +0200

    libe57: add version with cmake 4 support and c++ updates

--- packages/libe57/package.py
+++ packages/libe57/package.py
@@ -15,6 +15,8 @@ class Libe57(CMakePackage):
     maintainers = ['aumuell']
 
     version('master', preferred=False, branch='master')
+    version("1.1.337", sha256="093bf48e8be73af95778d1f587da0d10c6fa6c013d54431750400cb565f44f83")
+    version("1.1.336", sha256="a61d28adb2943aab61348c114bfb8a6f565386f9bca2695864ab53d33874e1f4")
     version("1.1.335", sha256="2e612aa0bb1c8f4659ca6dbcc8f918344e90cfa0334e16532016cfb24566f545")
     version("1.1.334", sha256="ff15ce1d2d9572daedbe990755800ea5fb0ef5dfbd8b9db9d679e73c43996bc4")
     version("1.1.333", sha256="f8fe8e329fb75c48bea5e45cad46cb011a62f3005cb1551cdc183e98253cfd44")
